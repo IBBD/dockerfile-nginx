9710b02fdb99b18bacbac9c1d2ef0ff8ec1ca6c4
alex cai <cyy0523xc@gmail.com>
2015-08-25 12:18:30 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ docker rm ibbd-nginx
 
 docker run --name=ibbd-nginx -d \
     -p 80:80 \
-    --link ibbd-php:php-fpm \
+    --link ibbd-php-fmp:php-fpm \
     -v /usr/local/nginx/conf/vhost:/etc/nginx/sites-enabled \
     -v /var/log/nginx:/var/log/nginx \
     -v /home/code/ibbd:/var/www \
-    --volumes-from ibbd-php \
+    --volumes-from ibbd-php-fpm \
     ibbd/nginx
 
 docker ps
