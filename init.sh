c2b13fadaedde115c06fddfebef6913f5f340667
alex cai <cyy0523xc@gmail.com>
2015-09-02 17:27:40 +0800

    init.sh

--- init.sh
+++ init.sh
@@ -2,10 +2,22 @@
 #
 # 初始化 
 
+create_dir() {
+    [ ! -d $1 ] && sudo mkdir -p $1
+}
+
+cp_example() {
+    [ ! -f $1 ] && cp $1".example" $1
+}
+
 # 初始化nginx的配置文件
-cp run.sh.example run.sh
-cp conf/nginx.conf.example   conf/nginx.conf
-cp conf/fastcgi.conf.example conf/fastcgi.conf
+cp_example run.sh
+cp_example conf/nginx.conf
+cp_example conf/fastcgi.conf
+
+# 创建相关目录
+create_dir /var/log/nginx
+create_dir /etc/nginx/sites-enabled/
 
 echo '==> nginx init is ok!'
 
