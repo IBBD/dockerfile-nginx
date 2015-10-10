11d0a513e784c1d735bd4083222dd68fdf8df153
alex <alex@ibbd.net>
2015-10-10 14:26:36 +0800

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop ibbd-nginx 
-docker rm ibbd-nginx 
-docker rmi ibbd/nginx 
+sudo docker stop ibbd-nginx 
+sudo docker rm ibbd-nginx 
+sudo docker rmi ibbd/nginx 
 
 # 重新生成
-docker build -t ibbd/nginx ./
+sudo docker build -t ibbd/nginx ./
 
